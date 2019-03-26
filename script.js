1886449e9095449febe9f4db6feaabd608d007e1
Gabriel Augusto Saraiva <48811062+gabrrieel@users.noreply.github.com>
2019-03-26 16:31:16 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-for(var i=0;i< json.empresas.length; i++){
-  var html = "<tr>";
-  html +="<td>"+json.empresas[i].author+"</td>";
-  html +="<td>"+json.empresas[i].title+"</td>";
-  html +="<td>"+json.empresas[i].url+"</td>";
-  html +="<td>"+json.empresas[i].upvotes+"</td>";
-  html +="</tr>";
-  $('table tbody').append(html);
-}
